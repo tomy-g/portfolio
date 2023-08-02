ab01e2753fd9d7631ee04a69e1aebf618ae6a7f7
Tomás <tomasgoizuetadp@gmail.com>
2023-08-02 18:51:06 +0200

    blur not working

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Nav = ({ pages }) => {
   return (
     <nav>
-      <ul>
+      <ul className="flex flex-row">
         {pages.map((page) => (
           <li>{page}</li>
         ))}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      'blue-primary': 'var(--blue-primary)',
+      'pink-primary': 'var(--pink-primary)',
+      'light-gray': 'var(--light-gray)',
+      'dark-gray': 'var(--dark-gray)',
+      'black-primary': 'var(--black-primary)',
+      'white-primary': 'var(--v)',
+
+    },
+    extend: {
+      fontFamily: {
+        sans: [ "Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
+
