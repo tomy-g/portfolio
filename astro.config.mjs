5ca5fdeb3894b172af8049b782b2e3b38fc0510c
Tomás <tomasgoizuetadp@gmail.com>
2024-01-07 18:04:44 +0100

    vercel fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), astroI18next()]
 });
