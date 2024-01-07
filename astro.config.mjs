78bac15e80d0d34d57e413bbd54bd946c7af0a1e
Tomás <tomasgoizuetadp@gmail.com>
2024-01-07 18:01:26 +0100

    fixed for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,8 @@
-import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
-import astroI18next from "astro-i18next";
+import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 3000,
-  },
-  integrations: [astroI18next(), react()],
+  integrations: [react()]
 });
