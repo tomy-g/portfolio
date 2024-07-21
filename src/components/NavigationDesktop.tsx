02880f1a4ccc1272942c40feea259742f80128c4
Tomás <tomasgoizuetadp@gmail.com>
2024-07-21 18:17:39 +0200

    feat: header

--- src/components/NavigationDesktop.tsx
+++ src/components/NavigationDesktop.tsx
@@ -1,25 +1,40 @@
 import "../styles/navigationDesktop.css";
-import { useState } from 'react'
+import { IconFileFilled, IconWorld } from "@tabler/icons-react";
+import LanguagePicker from "./LanguagePicker.astro";
+import { languages } from '../i18n/ui';
+import { getLangFromUrl, useTranslatedPath } from '../i18n/utils';
 
-const NavigationDesktop = ({ pages }) => {
+interface Props {
+  pages: string[],
+  lang: string
+}
+
+const NavigationDesktop = ({pages, lang}: Props) => {
+  const translatePath = useTranslatedPath(lang as "en" | "es");
+  
   return (
-      <nav className="desktop-nav">
-        <ul className="pages">
-          {pages.map((page) => (
-            <li key={page}>
-              <a>{page}</a>
-            </li>
-          ))}
-        </ul>
-        <ul className="icons">
-          <li>
-            <a>Es</a>
+    <nav className="desktop-nav">
+      <ul className="pages">
+        {pages.map((page) => (
+          <li key={page}>
+            <a>{page}</a>
           </li>
-          <li>
-            <a>CV</a>
-          </li>
-        </ul>
-      </nav>
+        ))}
+      </ul>
+      <ul className="icons">
+        <li>
+          <a className="translate" href={translatePath('/', lang)}>
+            <IconWorld color="var(--light-gray)" stroke={1.5} />
+            Es
+          </a>
+        </li>
+        <li>
+          <button className="resume" >
+            <IconFileFilled color="var(--light-gray)" height={21} />
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
