a7835d4066948a7f9ac267840d6e50c2f31025e3
Tomás <tomasgoizuetadp@gmail.com>
2024-08-10 13:45:08 +0200

    feat: icons hover

---
+++ src/constants/urls.ts
@@ -0,0 +1,14 @@
+export const URLS = {
+    REACT: 'https://react.dev',
+    ANGULAR: 'https://angular.dev/',
+    ASTRO: 'https://astro.build/',
+    CSS: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+    HTML: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+    JS: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    TS: 'https://www.typescriptlang.org/',
+    NODE: 'https://nodejs.org/en/',
+    SASS: 'https://sass-lang.com/',
+    NEXT: 'https://nextjs.org/',
+    FIGMA: 'https://www.figma.com/',
+    TAILWIND: 'https://tailwindcss.com/',
+}
