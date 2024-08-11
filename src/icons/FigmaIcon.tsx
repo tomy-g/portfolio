dbd15e19379ed19b6668ea7023e990b2492763f9
Tomás <tomasgoizuetadp@gmail.com>
2024-08-11 15:27:37 +0200

    feat: responsiveness and about gradient

--- src/icons/FigmaIcon.tsx
+++ src/icons/FigmaIcon.tsx
@@ -4,9 +4,9 @@ function FigmaIcon () {
   return (
     <svg
       className='logo'
-      width='26'
+      width='41'
       height='41'
-      viewBox='0 0 26 41'
+      viewBox='0 0 41 41'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
