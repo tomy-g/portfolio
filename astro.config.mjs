7129da40139ab0d3354c5b3632f72be5f5d8f470
Tomás <tomasgoizuetadp@gmail.com>
2024-08-13 16:36:32 +0200

    feat: add experience and vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import vercelServerless from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercelServerless(),
   i18n: {
     defaultLocale: "en",
     locales: ["es", "en"],
