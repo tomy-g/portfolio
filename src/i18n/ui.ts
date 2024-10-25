c2695625d4fc0a1e893422bec62a220f6b21e92c
Tomás <tomasgoizuetadp@gmail.com>
2024-10-25 19:35:01 +0200

    fix: responsive

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,28 +1,51 @@
 export const languages = {
-  en: "English",
-  es: "Español",
-};
+  en: 'English',
+  es: 'Español'
+}
 
-export const showDefaultLang = false;
+export const showDefaultLang = false
 
-export const defaultLang = "en";
+export const defaultLang = 'en'
 
 export const ui = {
   en: {
-    "SITE.TITLE": "Tomy Goizueta",
-    "SITE.DESCRIPTION":
-        "Portfolio of Tomy Goizueta, a software developer from Spain. Made in Astro",
-    "HOME.TITLE": "TOMY GOIZUETA",
-    "HOME.SUBTITLE": "This is a <0>more complex</0> string to translate, mixed with <1>html elements</1>, such as a <2>a cool link</2>!",
-    "NAVBAR.ABOUT": "About",
-    "NAVBAR.EXPERIENCE": "Exp",
-    "NAVBAR.PROJECTS": "Projects",
-    "NAVBAR.CONTACT": "Contact",
+    'SITE.TITLE': 'Tomy Goizueta',
+    'SITE.DESCRIPTION':
+      'Portfolio of Tomy Goizueta, a software developer from Spain. Made in Astro',
+    'NAVBAR.ABOUT': 'About',
+    'NAVBAR.EXPERIENCE': 'Exp',
+    'NAVBAR.PROJECTS': 'Projects',
+    'NAVBAR.CONTACT': 'Contact',
+    'HOME.TITLE': 'TOMY GOIZUETA',
+    'HOME.SUBTITLE':
+      'This is a <0>more complex</0> string to translate, mixed with <1>html elements</1>, such as a <2>a cool link</2>!',
+    'HOME.DESCRIPTION': `Software Engineer with 1 year of experience as Front-End developer. 
+      My lifelong passion for design has been combined with my interest insoftware development 
+      to create responsive and attractive products.`,
+    'HOME.HIGHLIGHT': 'Living in',
+    'HOME.LOCATION': 'Málaga',
+    'HOME.CTA': 'MY WORKS',
+    'ABOUT.TITLE': 'A little about me.',
+    'ABOUT.DESCRIPTION': ` Apasionado por el desarrollo y el diseño.
+            <br />
+            <br />
+            Estudiante autodidacta, continuamente aprendiendo y estando al día en
+            nuevas tecnologías.
+            <br />
+            <br />
+            Actualmente centrado en mejorar mis habilidades técnicas y estar al día
+            con nuevos frameworks como Next.js 14 o Astro.`,
+    'ABOUT.STUDIES': 'Ingeniería del Software en ',
+    'ABOUT.UNIVERSITY': 'UMA',
+    'ABOUT.TIMESTUDIES': '2019 - 2024',
+    'ABOUT.LASTROLE': 'Software Engineer Front en ',
+    'ABOUT.LASTCOMPANY': 'Babel',
+    'ABOUT.TIMECOMPANY': 'Feb 2023 - Feb 2024',
   },
   es: {
-    "NAVBAR.ABOUT": "Acerca",
-    "NAVBAR.EXPERIENCE": "Exp",
-    "NAVBAR.PROJECTS": "Proyectos",
-    "NAVBAR.CONTACT": "Contacto",
-  },
-} as const;
+    'NAVBAR.ABOUT': 'Acerca',
+    'NAVBAR.EXPERIENCE': 'Exp',
+    'NAVBAR.PROJECTS': 'Proyectos',
+    'NAVBAR.CONTACT': 'Contacto'
+  }
+} as const
